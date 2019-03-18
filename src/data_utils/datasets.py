3492f1de6aac335d3a4dcde805b3d642239366b7
Donald Hamnett <donald.hamnett@gmail.com>
2019-03-17 22:24:09 -0400

    removed bugs, added POC notebook

--- src/data_utils/datasets.py
+++ src/data_utils/datasets.py
@@ -1,9 +1,10 @@
 import pypianoroll
 import torch
+import torch.utils.data
 import numpy as np
 import os
 
-class MultiTrackPianoRollDataset(Dataset):
+class MultiTrackPianoRollDataset(torch.utils.data.Dataset):
     """Piano Roll dataset."""
 
     def __init__(self, root_dir, transform=None):
@@ -13,17 +14,18 @@ class MultiTrackPianoRollDataset(Dataset):
             transform (callable, optional): Optional transform to be applied
                 on a sample.
         """
+        self.root_dir = root_dir
         self.piano_rolls = []
-        self._populate_filenames(rootdir)
+        self._populate_filenames()
         
                     
-        self.root_dir = root_dir
+        
         self.transform = transform
         
-    def _populate_filenames(self, root_dir):
-        for root, _, files in os.walk(rootdir, topdown=False):
+    def _populate_filenames(self):
+        for root, _, files in os.walk(self.root_dir, topdown=False):
             for name in files:
-                if len(name) > 3 name[-4:] == '.npz'
+                if len(name) > 3 and name[-4:] == '.npz':
                     self.piano_rolls.append(os.path.join(root,name))
                     
     def __len__(self):
--- src/data_utils/transforms.py
+++ src/data_utils/transforms.py
@@ -1,23 +1,35 @@
 import numpy as np
+import torch
 
-class ToPadded(object):
+
+def to_numpy(multitrack_pianoroll, expected_tracks):
+    num_tracks = len(multitrack_pianoroll.tracks)
+    if expected_tracks is not None and num_tracks != expected_tracks:
+        raise RuntimeError(f'Given pianoroll with {num_tracks} tracks, expected {self.expected_tracks}')
+    return np.array([track.pianoroll for track in multitrack_pianoroll.tracks])
+
+
+def to_padded(tracks, pad_to, pad_token):
+    num_tracks, input_len, input_width = tracks.shape
+    if input_len == pad_to:
+        return tracks
+    if input_len > pad_to:
+        raise RuntimeError(f'Given pad_to {self.pad_to}, but input has length {input_len}')
+    padding = np.ones((num_tracks, pad_to - input_len, input_width)) * pad_token
+
+    return np.hstack((tracks, padding))
+
+def total_transform(pad_to=959040, pad_token=129, expected_tracks=5):
+    return lambda x : torch.tensor(to_padded(to_numpy(x, expected_tracks), pad_to, pad_token))
+
     
-    def __init__(self, pad_to, pad_token):
-        self.pad_to = pad_to
-        self.pad_token = pad_token
+
+    
+
         
-    def __call__(self, input_pianoroll):
-        multitrack_pianoroll = input_pianoroll.copy()
-        input_len, input_width = multitrack_pianoroll.tracks[0].pianoroll.shape
+    
+    
         
-        if input_len == self.pad_to:
-            return multitrack_pianoroll
-        if input_len > self.pad_to:
-            raise RuntimeError(f'Given pad_to {self.pad_to}, but input has length {input_len}')
-        padding = np.ones((self.pad_to - input_len, input_width)) * self.pad_token
-        
-        for track in multitrack_pianoroll.tracks:
-            track.pianoroll = np.vstack((track.pianoroll, padding))
         
             
             
